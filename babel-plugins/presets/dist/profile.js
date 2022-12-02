c2a78b91eff02ef792812b6fe069a61c383df736
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-02 20:44:34 +0100

    finished task 'babel-presets'

---
+++ babel-plugins/presets/dist/profile.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var printProfile = function printProfile(profileData) {
+  var name = profileData.name,
+    company = profileData.company;
+  console.log("".concat(name, " from ").concat(company));
+};
