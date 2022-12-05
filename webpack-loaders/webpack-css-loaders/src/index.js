426d74c560670fa0c796af124acbc203d123505a
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-06 00:46:03 +0100

    finished task 'webpack-css-loaders'

---
+++ webpack-loaders/webpack-css-loaders/src/index.js
@@ -0,0 +1,10 @@
+
+import renderProfile from './profile/renderProfile';
+import './index.css';
+
+const profileData = {
+  name: 'Tom',
+  location: 'The World',
+};
+
+renderProfile(profileData);
---
+++ webpack-loaders/webpack-css-loaders/webpack.config.js
@@ -0,0 +1,30 @@
+const isDev = process.argv[process.argv.indexOf('--mode') + 1] === 'development';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images'
+            }
+          }
+        ]
+      },
+    ]
+  },
+  watch: isDev,
+  devtool: isDev ? 'eval-source-map' : false
+}
