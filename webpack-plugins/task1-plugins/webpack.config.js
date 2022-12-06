2d982e1fd39231a1a416fa33e3480e2d87ed614f
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-06 19:15:51 +0100

    finished task 'webpack-plugins'

---
+++ webpack-plugins/task1-plugins/webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.argv[process.argv.indexOf('--mode') + 1] === 'development';
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images',
+              limit: 8192
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+  ],
+  watch: isDev,
+  devtool: isDev ? 'eval-source-map' : false
+}
