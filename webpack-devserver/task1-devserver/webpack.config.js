e9a39a39f889a1f85f9a76c7f14c61b05053982c
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-06 20:27:23 +0100

    finished task 'webpack-devserver'

---
+++ webpack-devserver/task1-devserver/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.argv[process.argv.indexOf('--mode') + 1] === 'development';
+
+const plugins = () => {
+  const base = [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    
+  ]
+
+  !isDev && (
+    base.push(
+      new MiniCssExtractPlugin({
+        filename: "[name].css"
+      })
+    )
+  )
+
+  return base
+}
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /.s?css$/,
+        use: [
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images',
+              limit: 8192
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: plugins(),
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    port: 9000,
+    inline: false,
+  },
+  watch: isDev,
+  devtool: isDev ? 'eval-source-map' : false
+}
