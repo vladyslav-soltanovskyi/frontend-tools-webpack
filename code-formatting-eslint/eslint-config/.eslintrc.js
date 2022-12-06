418241525a24722b4bc57741f880efa970b6b717
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2022-12-07 00:50:52 +0100

    finished task 'eslint-config'

---
+++ code-formatting-eslint/eslint-config/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "eslint-config-airbnb-base",
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "globals": {
+        "document": true,
+    },
+    "rules": {
+        "no-console": 0
+    }
+};
---
+++ code-formatting-eslint/eslint-config/src/index.js
@@ -0,0 +1,15 @@
+import printProfile from './renderProfile';
+
+const userData = {
+  name: 'Tom',
+  age: 17,
+};
+
+const profile = {
+  ...userData,
+  company: 'Gromcode',
+};
+
+printProfile(profile);
+
+console.log(profile);
